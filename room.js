27a63921570d863cd4a2c37ba1c34dd071d4c830
LERUfic <aguelsw@gmail.com>
2017-09-30 01:19:53 +0700

    Menambah library room.js

---
+++ room.js
@@ -0,0 +1,14 @@
+function Room(name, id, owner) {
+  this.id = id;
+  this.people = [];
+  this.owner = owner;
+  this.status = "available";
+};
+
+Room.prototype.addPerson = function(personID) {
+  if (this.status === "available") {
+    this.people.push(personID);
+  }
+};
+
+module.exports = Room;
